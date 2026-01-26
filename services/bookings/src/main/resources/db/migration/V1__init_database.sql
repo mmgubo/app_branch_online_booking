a129a68f1878437c3d1102717357dced3a486726
Mfundo Mgubo <mfundomgubo@Mfundos-MacBook-Air.local>
2026-01-26 11:39:34 +0200

    update sql init script for bookings and branch service

--- services/bookings/src/main/resources/db/migration/V1__init_database.sql
+++ services/bookings/src/main/resources/db/migration/V1__init_database.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS customer_bookings
 (
     id                  SERIAL PRIMARY KEY,
-    reference           VARCHAR(255) NOT NULL UNIQUE,
     customer_id         VARCHAR(255),
     created_date        TIMESTAMP NOT NULL,
-    last_modified_date  TIMESTAMP
+    time                VARCHAR(255),
+    status              VARCHAR(255),
+    branch              VARCHAR(255),
+    notes               VARCHAR(255),
+    service             VARCHAR(255),
+    date                VARCHAR(255)
 );
 
--- services/branch/src/main/resources/db.migration/V1__init_database.sql
+++ services/branch/src/main/resources/db.migration/V1__init_database.sql
@@ -2,8 +2,6 @@ CREATE TABLE IF NOT EXISTS branch
 (
     id                  SERIAL PRIMARY KEY,
     bookings_id           VARCHAR(255) NOT NULL UNIQUE,
-    booking_reference      VARCHAR(50),
-    customer_id         VARCHAR(255),
     created_date        TIMESTAMP NOT NULL,
     last_modified_date  TIMESTAMP
     );
